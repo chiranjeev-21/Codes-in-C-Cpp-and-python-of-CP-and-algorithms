0259214bf1789fba4675ce3d52aa583b0a2fe2a2
Chiranjeev Singh <89395432+chiranjeev-21@users.noreply.github.com>
2023-08-29 15:07:49 +0530

    Update count-primes.cpp

--- Leetcode/204-count-primes/count-primes.cpp
+++ Leetcode/204-count-primes/count-primes.cpp
@@ -16,3 +16,33 @@ public:
         return cnt;
     }
 };
+
+
+//Using additional vector for dp
+class Solution {
+public:
+    int countPrimes(int n) {
+        vector<bool>vec(n+2,true);
+        vec[0]=false;
+        vec[1]=false;
+        vector<int>dp(n+2,0);
+        for(int i=2;i<n;i++){
+            if(vec[i]){
+                for(int j=2*i;j<n;j+=i){
+                    vec[j]=false;
+                }
+            }
+        }
+        int cnt=0;
+        for(int i=1;i<n+2;i++){
+            dp[i]=dp[i-1]+vec[i];
+        }
+        // for(auto i :dp){
+        //     cout<<i<<" ";
+        // }
+        if(vec[n]){
+            return dp[n]-1;
+        }
+        return dp[n];
+    }
+};
