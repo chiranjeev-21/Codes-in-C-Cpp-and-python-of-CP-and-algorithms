8e1b4ba95d14a624749b7e49e7fa6fec8ec1a59c
Chiranjeev Singh <89395432+chiranjeev-21@users.noreply.github.com>
2023-09-06 14:14:49 +0530

    Time: 7 ms (70.84%) | Memory: 11.2 MB (88.49%) - LeetSync

--- Leetcode/138-copy-list-with-random-pointer/copy-list-with-random-pointer.cpp
+++ Leetcode/138-copy-list-with-random-pointer/copy-list-with-random-pointer.cpp
@@ -16,22 +16,74 @@ public:
 
 class Solution {
 public:
+
+    //1) Solution using hashmaps
+
+    // Node* copyRandomList(Node* head) {
+    //     unordered_map<Node*,Node*> map;
+    //     Node* temp = head;
+    //     while(temp) {
+    //         map[temp] = new Node(temp->val);
+    //         temp=temp->next;
+    //     }
+
+    //     temp = head;
+    //     while(temp) {
+    //         Node* copy = map[temp];
+    //         copy->next = map[temp->next];
+    //         copy->random = map[temp->random];
+    //         temp = temp->next;
+    //     }
+
+    //     return map[head];
+    // }
+
+
+    //2) O(1) Space solution
+
     Node* copyRandomList(Node* head) {
-        unordered_map<Node*,Node*> map;
-        Node* temp = head;
-        while(temp) {
-            map[temp] = new Node(temp->val);
-            temp=temp->next;
+        if(!head) return nullptr;
+
+        Node* original = head; // itr for original
+        Node* cloned = nullptr; // itr for cloed
+        Node* temp = nullptr; // temporary for node creation
+
+        // First pass: clone nodes and link them next to origial
+        while(original) {
+            temp = new Node(original->val);
+            temp->next = original->next;
+            original->next=temp;
+            original = temp->next;
         }
 
-        temp = head;
-        while(temp) {
-            Node* copy = map[temp];
-            copy->next = map[temp->next];
-            copy->random = map[temp->random];
-            temp = temp->next;
+        original = head;
+        Node* newHead = head->next;
+
+        // Second pass: Assign random pointers to cloned nodes
+        while(original) {
+            if(original->random) {
+                original->next->random = original->random->next;
+            } else {
+                original->next->random = nullptr;
+            }
+            original = original->next->next; // skip copy
+        }
+        
+        original = head;
+
+        // Third Pass: Seperate original and cloned 
+        while(original) {
+            temp = original->next; 
+            original->next = temp->next; 
+            original = original->next;
+
+            if(original) {
+                temp->next = original->next;
+            } else {
+                temp->next = nullptr;
+            }
         }
 
-        return map[head];
+    return newHead;
     }
 };
