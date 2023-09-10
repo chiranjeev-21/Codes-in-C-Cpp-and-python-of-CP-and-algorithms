204b918319a14c2aae976de56b4132f270156755
Chiranjeev Singh <89395432+chiranjeev-21@users.noreply.github.com>
2023-09-11 02:09:27 +0530

    Time: 3 ms (99.02%) | Memory: 10.4 MB (88.24%) - LeetSync

---
+++ Leetcode/300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int lengthOfLIS(vector<int>& nums) {
+        int n = nums.size();
+        vector<int>vec;
+        vec.push_back(nums[0]);
+        for(int i=1;i<n;i++){
+            if(vec[vec.size()-1]<nums[i]){
+                vec.push_back(nums[i]);
+            }
+            else{
+                auto idx = lower_bound(vec.begin(),vec.end(),nums[i]);
+                *idx = nums[i];
+            }
+        }
+    return vec.size();
+    }
+};
