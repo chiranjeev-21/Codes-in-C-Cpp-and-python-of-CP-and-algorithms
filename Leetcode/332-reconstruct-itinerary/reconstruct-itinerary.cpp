f7d004230d92874c1d6c249b1d92a44df908fee4
Chiranjeev Singh <89395432+chiranjeev-21@users.noreply.github.com>
2023-09-14 20:06:23 +0530

    Time: 34 ms (18.77%) | Memory: 14.3 MB (66.78%) - LeetSync

---
+++ Leetcode/332-reconstruct-itinerary/reconstruct-itinerary.cpp
@@ -0,0 +1,37 @@
+class Solution {
+private:
+    void dfs(unordered_map<string,priority_queue<string , vector<string> , greater<string>>>& mp, string src, vector<string>& ans) {
+        while (mp.find(src) != mp.end() && !mp[src].empty()) {
+            string next = mp[src].top();
+            mp[src].pop();
+            dfs(mp, next, ans);
+        }
+        ans.insert(ans.begin(), src);
+    }
+
+public:
+    vector<string> findItinerary(vector<vector<string>>& tickets) {
+        vector<string> ans;
+        unordered_map<string, priority_queue<string , vector<string> , greater<string>>> mp;
+        for (vector<string>& x : tickets) {
+            string from = x[0];
+            string to = x[1];
+            if (mp.find(from) == mp.end()) {
+                mp[from] = priority_queue<string , vector<string> , greater<string>>();
+            }
+            mp[from].push(to);
+        }
+        // for(auto i :g){
+        //     cout<<i.first<<" -> ";
+        //     int sz=i.second.size();
+        //     while(sz--){
+        //         cout<<i.second.top()<<" ";
+        //         i.second.pop();
+        //     }
+        //     cout<<"\n";
+        // }
+        string src = "JFK";
+        dfs(mp, src, ans);
+        return ans;
+    }
+};
