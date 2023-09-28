5eb66654f7a5e04b954a8794bc86b726658c0955
Chiranjeev Singh <89395432+chiranjeev-21@users.noreply.github.com>
2023-09-28 13:11:30 +0530

    Time: 7 ms (66.47%) | Memory: 16.7 MB (18.22%) - LeetSync

--- Leetcode/941-sort-array-by-parity/sort-array-by-parity.cpp
+++ Leetcode/941-sort-array-by-parity/sort-array-by-parity.cpp
@@ -1,22 +1,27 @@
 class Solution {
 public:
     vector<int> sortArrayByParity(vector<int>& nums) {
-        int left=0,right=1;
-        // [0,1]
-        while(right<nums.size()){
-            if(nums[left]%2==0){
-                left++;
-                right++;
-            }
-            else if(nums[left]%2!=0 && nums[right]%2==0){
-                swap(nums[left],nums[right]);
-                left++;
-                right++;
-            }
-            else if(nums[left]%2!=0 && nums[right]%2!=0){
-                right++;
-            }
-        }
+
+        sort(nums.begin(), nums.end(), [](int a, int b) {
+            return (a % 2) < (b % 2);
+        });
+
+        // int left=0,right=1;
+        // // [0,1]
+        // while(right<nums.size()){
+        //     if(nums[left]%2==0){
+        //         left++;
+        //         right++;
+        //     }
+        //     else if(nums[left]%2!=0 && nums[right]%2==0){
+        //         swap(nums[left],nums[right]);
+        //         left++;
+        //         right++;
+        //     }
+        //     else if(nums[left]%2!=0 && nums[right]%2!=0){
+        //         right++;
+        //     }
+        // }
         return nums;
     }
 };
