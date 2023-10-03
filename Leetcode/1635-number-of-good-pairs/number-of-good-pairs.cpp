ecfea80a682a8eb1b17efc58bbe0901c8455540e
Chiranjeev Singh <89395432+chiranjeev-21@users.noreply.github.com>
2023-10-04 02:09:37 +0530

    Time: 3 ms (42.79%) | Memory: 7.6 MB (7.35%) - LeetSync

---
+++ Leetcode/1635-number-of-good-pairs/number-of-good-pairs.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int numIdenticalPairs(vector<int>& nums) {
+        unordered_map<int,int>mp;
+        for(int i=0;i<nums.size();i++){
+            mp[nums[i]]++;
+        }
+        int ans=0;
+        for(auto i :mp){
+            ans+=(i.second*(i.second-1)/2);
+        }
+        return ans;
+    }
+};
