e5a4a401eaf4a9656470ddcb647f86aa7b323094
Chiranjeev Singh <89395432+chiranjeev-21@users.noreply.github.com>
2023-10-10 00:16:03 +0530

    Time: 3 ms (91.96%) | Memory: 14 MB (25.30%) - LeetSync

--- Leetcode/34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.cpp
+++ Leetcode/34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,44 +1,51 @@
 class Solution {
 public:
-    vector<int> searchRange(vector<int>& nums, int target) {
-        int n=nums.size();
-        int start=0,end=n-1;
-        int res=-1,res1=-1;
-        if(nums.size()==0){
+    vector<int> searchRange(vector<int>& nums, int tar) {
+        if(binary_search(nums.begin(),nums.end(),tar)==false)
             return {-1,-1};
-        }
-        if(nums.size()==1 && nums[0]==target){
-            return {0,0};
-        }
-        while(start<=end){
-            int mid=start + (end-start)/2;
-            if(nums[mid]==target){
-                res=mid;
-                end=mid-1;
-            }
-            else if(nums[mid]<target){
-                start=mid+1;
-            }
-            else if(nums[mid]>target){
-                end=mid-1;
-            }
-        }
-        start=0;
-        end=n-1;
-        while(start<=end){
-            int mid=start + (end-start)/2;
-            if(nums[mid]==target){
-                res1=mid;
-                start=mid+1;
-            }
-            else if(nums[mid]>target){
-                end=mid-1;
-            }
-            else if(nums[mid]<target){
-                start=mid+1;
-            }
-        }
-        // cout<<res<<" ";
-        return {res,res1};
+        int lb = lower_bound(nums.begin(), nums.end(), tar) - nums.begin();
+        int ub = upper_bound(nums.begin(),nums.end(),tar)-nums.begin()-1;
+        return {lb, ub};
     }
+    // vector<int> searchRange(vector<int>& nums, int target) {
+    //     int n=nums.size();
+    //     int start=0,end=n-1;
+    //     int res=-1,res1=-1;
+    //     if(nums.size()==0){
+    //         return {-1,-1};
+    //     }
+    //     if(nums.size()==1 && nums[0]==target){
+    //         return {0,0};
+    //     }
+    //     while(start<=end){
+    //         int mid=start + (end-start)/2;
+    //         if(nums[mid]==target){
+    //             res=mid;
+    //             end=mid-1;
+    //         }
+    //         else if(nums[mid]<target){
+    //             start=mid+1;
+    //         }
+    //         else if(nums[mid]>target){
+    //             end=mid-1;
+    //         }
+    //     }
+    //     start=0;
+    //     end=n-1;
+    //     while(start<=end){
+    //         int mid=start + (end-start)/2;
+    //         if(nums[mid]==target){
+    //             res1=mid;
+    //             start=mid+1;
+    //         }
+    //         else if(nums[mid]>target){
+    //             end=mid-1;
+    //         }
+    //         else if(nums[mid]<target){
+    //             start=mid+1;
+    //         }
+    //     }
+    //     // cout<<res<<" ";
+    //     return {res,res1};
+    // }
 };
