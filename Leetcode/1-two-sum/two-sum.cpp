13b50695c53f4b3614eb29e73ee43ce1381306b9
Chiranjeev Singh <89395432+chiranjeev-21@users.noreply.github.com>
2023-10-16 04:01:38 +0530

    Time: 6 ms (92.28%) | Memory: 11.1 MB (29.93%) - LeetSync

--- Leetcode/1-two-sum/two-sum.cpp
+++ Leetcode/1-two-sum/two-sum.cpp
@@ -1,13 +1,19 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        unordered_map<int, int> indices;
-        for (int i = 0; i < nums.size(); i++) {
-            if (indices.find(target - nums[i]) != indices.end()) {
-                return {indices[target - nums[i]], i};
+        ios_base::sync_with_stdio(false);
+        unordered_map<int,int> mp;
+        for(int i=0;i<nums.size();i++)
+        {
+            if(mp.find(target-nums[i]) != mp.end())
+            {
+                return {mp[target-nums[i]], i};
             }
-            indices[nums[i]] = i;
+            else
+            {
+            mp[nums[i]]=i;
+            }
         }
-        return {};
+        return {-1,-1};
     }
 };
