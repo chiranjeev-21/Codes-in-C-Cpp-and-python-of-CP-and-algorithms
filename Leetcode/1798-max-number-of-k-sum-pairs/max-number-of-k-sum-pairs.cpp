c76433b3e4baf54021747b1e0ec5f39e89ded8a2
Chiranjeev Singh <89395432+chiranjeev-21@users.noreply.github.com>
2023-10-22 23:36:00 +0530

    Time: 270 ms (5.06%) | Memory: 73.1 MB (12.47%) - LeetSync

--- Leetcode/1798-max-number-of-k-sum-pairs/max-number-of-k-sum-pairs.cpp
+++ Leetcode/1798-max-number-of-k-sum-pairs/max-number-of-k-sum-pairs.cpp
@@ -1,22 +1,38 @@
 class Solution {
 public:
     int maxOperations(vector<int>& nums, int k) {
-        sort(nums.begin(),nums.end());
+        //Hash table
+        int cnt=0;
+        unordered_map<int,int>mp;
         int n=nums.size();
-        int low=0,high=n-1,cnt=0;
-        while(low<high){
-            if(nums[low]+nums[high]==k){
+        for(int i=0;i<n;i++){
+            if(mp[k-nums[i]]>0){
                 cnt++;
-                low++;
-                high--;
-            }
-            else if(nums[low]+nums[high]>k){
-                high--;
+                mp[k-nums[i]]--;
             }
             else{
-                low++;
+                mp[nums[i]]++;
             }
         }
         return cnt;
+
+        //sorting and 2 pointers
+        // sort(nums.begin(),nums.end());
+        // int n=nums.size();
+        // int low=0,high=n-1,cnt=0;
+        // while(low<high){
+        //     if(nums[low]+nums[high]==k){
+        //         cnt++;
+        //         low++;
+        //         high--;
+        //     }
+        //     else if(nums[low]+nums[high]>k){
+        //         high--;
+        //     }
+        //     else{
+        //         low++;
+        //     }
+        // }
+        // return cnt;
     }
 };
