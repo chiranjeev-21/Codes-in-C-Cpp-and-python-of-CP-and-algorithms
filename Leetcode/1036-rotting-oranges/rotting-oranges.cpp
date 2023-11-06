2d84a13f2d79a72260d391bff5ef4ed09483730d
Chiranjeev Singh <89395432+chiranjeev-21@users.noreply.github.com>
2023-11-06 16:06:50 +0530

    Time: 3 ms (87.97%) | Memory: 13.5 MB (47.98%) - LeetSync

--- Leetcode/1036-rotting-oranges/rotting-oranges.cpp
+++ Leetcode/1036-rotting-oranges/rotting-oranges.cpp
@@ -21,29 +21,28 @@ public:
         int ans=0;
         vector<int>dx={-1,0,0,1};
         vector<int>dy={0,1,-1,0};
-        vector<vector<bool>>vis(n,vector<bool>(m,0));
         while(!q.empty()){
-            ans++;
             int sz=q.size();
             for(int i=0;i<sz;i++){
                 auto temp=q.front();
                 int x= temp.first;
                 int y=temp.second;
-                vis[x][y]=true;
+                // vis[x][y]=true;
                 q.pop();
                 for(int i=0;i<4;i++){
                     int new_dx=x+dx[i];
                     int new_dy=y+dy[i];
-                    if(new_dx>=0 && new_dx<n && new_dy>=0 && new_dy<m && grid[new_dx][new_dy]==1 && !vis[new_dx][new_dy]){
+                    if(new_dx>=0 && new_dx<n && new_dy>=0 && new_dy<m && grid[new_dx][new_dy]==1 ){
                         grid[new_dx][new_dy]=2;
                         q.push({new_dx,new_dy});
                     }
                 }
             }
+            ans++;
         }
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
-                if(vis[i][j]==0 && grid[i][j]==1){
+                if(grid[i][j]==1){
                     return -1;
                 }
             }
