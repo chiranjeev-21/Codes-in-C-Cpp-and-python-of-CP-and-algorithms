bcc0c9a389771b2a3381972e49d9c4a84fbcced4
Chiranjeev Singh <89395432+chiranjeev-21@users.noreply.github.com>
2023-11-15 00:21:47 +0530

    Time: 0 ms (100.00%) | Memory: 7.6 MB (96.09%) - LeetSync

--- Leetcode/140-word-break-ii/word-break-ii.cpp
+++ Leetcode/140-word-break-ii/word-break-ii.cpp
@@ -1,10 +1,14 @@
+#include <iostream>
+#include <vector>
+#include <unordered_set>
+
 class Solution {
 public:
     static bool cmp(string& a, string& b) {
         return a.size() < b.size();
     }
 
-    void f(int start, int n, string& s, unordered_set<string>& dict, vector<string>& ans, string& temp, vector<vector<bool>>& dp) {
+    void wordBreakHelper(int start, int n, string& s, unordered_set<string>& dict, vector<string>& ans, string& temp, vector<bool>& dp) {
         if (start == n) {
             ans.push_back(temp.substr(0, temp.size() - 1));  // Remove the trailing space
             return;
@@ -12,29 +16,32 @@ public:
 
         for (int end = start; end < n; ++end) {
             string word = s.substr(start, end - start + 1);
-            if (dict.find(word) != dict.end() && dp[start][end]) {
+            if (dict.find(word) != dict.end() && dp[start]) {
                 string oldTemp = temp;
                 temp += word + " ";
-                f(end + 1, n, s, dict, ans, temp, dp);
+                wordBreakHelper(end + 1, n, s, dict, ans, temp, dp);
                 temp = oldTemp;
             }
         }
     }
-
     vector<string> wordBreak(string s, vector<string>& wordDict) {
         vector<string> ans;
         unordered_set<string> wordSet(wordDict.begin(), wordDict.end());
         int n = s.length();
         string temp = "";
-        vector<vector<bool>> dp(n, vector<bool>(n, false));  // dp[i][j] is true if substring s[i:j] can be segmented
-        for (int i = 0; i < n; ++i) {
-            for (int j = i; j < n; ++j) {
-                if (wordSet.find(s.substr(i, j - i + 1)) != wordSet.end()) {
-                    dp[i][j] = true;
+        vector<bool> dp(n + 1, false);  // dp[i] is true if substring s[0:i-1] can be segmented
+        dp[0] = true;
+        for (int i = 1; i <= n; ++i) {
+            for (int j = 0; j < i; ++j) {
+                if (dp[j] && wordSet.find(s.substr(j, i - j)) != wordSet.end()) {
+                    dp[i] = true;
+                    break;
                 }
             }
         }
-        f(0, n, s, wordSet, ans, temp, dp);
+        if (dp[n]){
+            wordBreakHelper(0, n, s, wordSet, ans, temp, dp);
+        }
         return ans;
     }
 };
