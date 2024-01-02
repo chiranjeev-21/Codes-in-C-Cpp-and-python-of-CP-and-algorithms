2dbfed7b229c90f51b116df85351e32d409dadf1
Chiranjeev Singh <89395432+chiranjeev-21@users.noreply.github.com>
2024-01-03 00:19:39 +0530

    Time: 30 ms (5.35%) | Memory: 30.3 MB (45.69%) - LeetSync

--- Leetcode/2724-convert-an-array-into-a-2d-array-with-conditions/convert-an-array-into-a-2d-array-with-conditions.cpp
+++ Leetcode/2724-convert-an-array-into-a-2d-array-with-conditions/convert-an-array-into-a-2d-array-with-conditions.cpp
@@ -1,32 +1,24 @@
 class Solution {
 public:
-
-    vector<int>check(unordered_map<int,int>& mp){
-        vector<int>temp;
-        for(auto i : mp){
-            if(i.second>0){
-                temp.push_back(i.first);
-                mp.at(i.first)=i.second-1;
-            }
-        }
-        return temp;
-    }
-
     vector<vector<int>> findMatrix(vector<int>& nums) {
-        int n=nums.size();
-        unordered_map<int,int>mp;
-        for(int i=0;i<n;i++){
-            mp[nums[i]]++;
-        }
-        vector<vector<int>>ans;
-        while(1){
-            vector<int>vec=check(mp);
-            if(vec.size()==0){
-                break;
+        vector<vector<int>> ans;
+        unordered_map<int, int> mp;
+        for(auto i: nums){
+            mp[i]++;
+        }  
+
+        while(!mp.empty()){
+            vector<int> temp;
+            for(auto it = mp.begin(); it != mp.end(); ){
+                temp.push_back(it->first);
+                it->second--;
+                if(it->second == 0){
+                    it = mp.erase(it);
+                } else {
+                    ++it;
+                }
             }
-            else{
-                ans.push_back(vec);
-            }
+            ans.push_back(temp);
         }
         return ans;
     }
