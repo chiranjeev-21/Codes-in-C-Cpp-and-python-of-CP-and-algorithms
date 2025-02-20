34bb73ce4896aa8b5d31c16ccc593a86438502b0
Chiranjeev Singh <89395432+chiranjeev-21@users.noreply.github.com>
2025-02-20 23:12:54 +0530

    [117] [Time Beats: 85.71%] [Memory Beats: 77.57%] - LeetPush

---
+++ Leetcode/117. Populating Next Right Pointers in Each Node II/117-Populating-Next-Right-Pointers-in-Each-Node-II.cpp
@@ -0,0 +1,68 @@
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    Node* left;
+    Node* right;
+    Node* next;
+
+    Node() : val(0), left(NULL), right(NULL), next(NULL) {}
+
+    Node(int _val) : val(_val), left(NULL), right(NULL), next(NULL) {}
+
+    Node(int _val, Node* _left, Node* _right, Node* _next)
+        : val(_val), left(_left), right(_right), next(_next) {}
+};
+*/
+
+class Solution {
+public:
+    Node* connect(Node* root) {
+
+        //BFS - O(N) Space and Time complexity
+        // if(!root)
+        //     return NULL;
+        
+        // queue<Node* > q;
+        // q.push(root);
+        // while(!q.empty()){
+        //     int n = q.size();
+        //     for(int i=0;i<n;i++){
+        //         Node* temp = q.front();
+        //         q.pop();
+        //         if(i!=n-1){
+        //             temp->next = q.front();
+        //         }else{
+        //             temp->next = NULL;
+        //         }
+        //         if(temp->left ){
+        //             q.push(temp->left);
+        //         }if(temp->right)
+        //             q.push(temp->right);   
+        //     }
+        // }
+        // return root;
+
+
+        //DFS - OPTIMAL SOLUTION - O(N) AND O(1) , Time and Space Complexity
+        Node* head=root;
+        while(head!=NULL){
+            Node* dummy = new Node(0);
+            Node* temp = dummy;
+            while(head!=NULL){
+                if(head->left){
+                    temp->next=head->left;
+                    temp=temp->next;
+                }
+                if(head->right){
+                    temp->next=head->right;
+                    temp=temp->next;
+                }
+                head=head->next;
+            }
+            head=dummy->next;
+        }
+        return root;
+    }
+};
